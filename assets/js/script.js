45ea642c3900e01b74761040dbd912497e437b4a
unknown <dvpamarasinghe31@gmail.com>
2023-09-10 09:31:25 +0530

    add placeholders

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,11 @@ var swiper = new Swiper(".mySwiper", {
     },
     loop:true,
   });
+
+  
+$(function(){
+  $("#nav-placeholder").load("assets/placeholders/nav.html");
+});
+$(function(){
+  $("#footer-placeholder").load("assets/placeholders/footer.html");
+});
