5b515939fb2b4ccfe4b5e60cdcf70d314f9050bf
unknown <dvpamarasinghe31@gmail.com>
2023-09-10 18:14:05 +0530

    Make the destination page

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,19 @@ var swiper = new Swiper(".mySwiper", {
     loop:true,
   });
 
+  var swiper = new Swiper(".DestinationSwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    loop:true,
+  });
   
 $(function(){
   $("#nav-placeholder").load("assets/placeholders/nav.html");
